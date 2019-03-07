16b27d249a71c65d831bdafca2bcc56b0f99563d
Wei Junyu <wei_junyu@tech.gov.sg>
2019-03-07 16:58:45 +0800

    Changed structure.
    Using express router so it's more understandable.
    Using helmet.
    Endpoints now served at /api.
    Added more sophisticated error handling, including 404.

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 const PORT = process.env.PORT || 5000;
-const Router = require('./routes');
+const routes = require('./routes');
 const app = express();
 
+app.use(helmet()); // Some security defaults. Check on npm for details
 app.use(morgan('combined'));
+app.use(cors());
 
-Router(app)
-    .use(cors())
-    .use((err, req, res, next) => {
-        res.status(500).send({ error: err.message });
-    })
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+    let status = err.status || 500;
+    res.status(status).send({ error: err.message });
+});
+
+// 404 handler, request has passed through all middleware and has not been processed.
+app.use((req, res, next) => {
+    res.status(404).send({
+        error: 'Not found.'
+    });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- routes.js
+++ routes.js
@@ -1,46 +1,65 @@
 const _ = require('lodash');
 const crypto = require('crypto');
+const express = require('express');
+const router = express.Router();
 
 const NAME = require('./package.json').name;
 const VERSION = require('./package.json').version;
-module.exports = Router;
-/**
- * @param {*} app Express app instance
- * @returns {*} app instance passed in, with all routes defined.
- */
-function Router(app) {
-    app.get('/', (req, res) => {
+
+router.get('/', (req, res) => {
+    res.json({
+        name: NAME,
+        version: VERSION
+    });
+});
+
+router.get('/echo', (req, res) => {
+    let interval = _.get(req, 'query.interval');
+    if (!interval) {
+        throw {
+            status: 400,
+            message: 'Specify response interval in ms.'
+        };
+    }
+    setTimeout(() => {
         res.json({
-            name: NAME,
-            version: VERSION
+            interval: interval + 'ms'
         });
+    }, interval);
+});
+router.get('/payload', (req, res) => {
+    let size = _.get(req, 'query.size');
+    if (!size) {
+        throw {
+            status: 400,
+            message: 'Specify a response size in bytes.'
+        };
+    }
+
+    let sizeInt = parseInt(size);
+    if (Number.isNaN(sizeInt)) {
+        throw {
+            status: 400,
+            message: 'Response size specified is not an integer.'
+        };
+    }
+    if (sizeInt < 0) {
+        throw {
+            status: 400,
+            message: `Specified payload size should be a positive integer. Received ${sizeInt}`
+        };
+    }
+
+    // Divide by 2 for more accurate payload size
+    let payload = crypto.randomBytes(parseInt(size / 2)).toString('hex');
+    res.json({
+        payload: payload
     });
-    app.get('/echo', (req, res) => {
-        let interval = _.get(req, 'query.interval');
-        if (!interval) {
-            throw new Error('Specify response interval in ms.');
-        }
-        setTimeout(() => {
-            res.json({
-                interval: interval + 'ms'
-            });
-        }, interval);
+});
+router.get('/headers', (req, res) => {
+    res.json({
+        headers: req.headers
     });
-    app.get('/payload', (req, res) => {
-        let size =_.get(req, 'query.size'); 
-        if (!size) {
-            throw new Error('Specify a response size in bytes.');
-        }
-        // Divide by 2 for more accurate payload size
-        let payload = crypto.randomBytes(parseInt(size / 2)).toString('hex');
-        res.json({
-            payload: payload
-        });
-    });
-    app.get('/headers', (req, res) => {
-        res.json({
-            headers: req.headers
-        });
-    });
-    return app;
-}
+});
+
+module.exports = router;
